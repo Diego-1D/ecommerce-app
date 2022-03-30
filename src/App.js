edf6e6d4621dd8e5b6282b552f65737c13e23a6a
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-29 23:25:05 -0300

    Criação das rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+import Product from "./components/Product";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <><BrowserRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
