e09bcd0f02237563ff2c6fa2e66cb2b933e5dec8
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-06 23:48:40 -0300

    Alterações no componente Products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Skeleton from 'react-loading-skeleton';
 import { NavLink } from 'react-router-dom';
+import { Card, Button, Col, Container } from 'react-bootstrap';
+
 
 const Products = () => {
 
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState(data);
     const [loading, setLoading] = useState(false);
+    const [active, setActive] = useState('todos');
 
     let componentMounted = true;
 
@@ -47,34 +50,59 @@ const Products = () => {
         )
     }
 
-    const filterProduct = (cat) => {
-        const updatedList = data.filter((e) => e.category === cat);
-        setFilter(updatedList);
-    }
+
 
     const ShowProducts = () => {
+        const filterProduct = (cat) => {
+            setActive(cat);
+            const updatedList = data.filter((e) => e.category === cat);
+            if (cat == 'todos') {
+                setFilter(data)
+            } else {
+                setFilter(updatedList);
+            }
+        }
         return (
             <>
-                <div className='buttons d-flex justify-content-center mb-5 pb-5'>
-                    <button className='btn btn-outline-dark me-2' onClick={() => setFilter(data)}>All</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("jewelery")}>Jewelery</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("eletronics")}>Eletronics</button>
-                </div>
-                {data.map((product) => {
+                <Container className='buttons d-flex justify-content-center mb-5 pb-5'>
+                    <Button
+                        variant='outline-dark me-2'
+                        className={active === "todos" && 'btn-outline-dark active me-2'}
+                        onClick={() => filterProduct("todos")}>Todos</Button>
+                    <Button
+                        variant='outline-dark me-2'
+                        className={active === "masculino" && 'btn-outline-dark active '}
+                        onClick={() => filterProduct("masculino")}>Masculino</Button>
+                    <Button
+                          variant='outline-dark me-2'
+                        className={active === "feminino" && 'btn-outline-dark active me-2'}
+                        onClick={() => filterProduct("feminino")}>Feminino</Button>
+                    <Button
+                          variant='outline-dark me-2'
+                        className={active === "joias" && 'btn-outline-dark active me-2'}
+                        onClick={() => filterProduct("joias")}>Jóias</Button>
+                    <Button
+                          variant='outline-dark me-2'
+                        className={active === "eletronicos" && 'btn-outline-dark active me-2'}
+                        onClick={() => filterProduct("eletronicos")}>Eletrônicos</Button>
+                </Container>
+                {filter.map((product) => {
+                    let title = product.title;
+                    if (title.length > 14) {
+                        title = title.substring(0, 14) + `...`;
+                    }
                     return (
                         <>
-                            <div className='col-md-3 mb-4'>
-                                <div class="card h-100 text-center p-4" key={product.id}>
-                                    <img src={product.image} class="card-img-top" alt={product.title} height='250px' />
-                                    <div class="card-body">
-                                        <h5 class="card-title mb-0">{product.title}</h5>
-                                        <p class="card-text lead fw-bold">${product.price}</p>
-                                        <NavLink to={`products/${product.id}`} class="btn btn-outline-dark">Buy Now</NavLink>
-                                    </div>
-                                </div>
-                            </div>
+                            <Col className='md-4 mb-4 d-flex justify-content-center'>
+                                <Card className="text-center p-3" style={{ width: '15rem' }} key={product.id}>
+                                    <Card.Img variant="top" src={product.image} alt={product.title} height='200px' />
+                                    <Card.Body>
+                                        <Card.Title className='mb-0'>{title}</Card.Title>
+                                        <Card.Text className='fw-bold'>${product.price}</Card.Text>
+                                        <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Comprar agora</NavLink>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         </>
                     )
                 })
@@ -86,10 +114,10 @@ const Products = () => {
 
     return (
         <div>
-            <div className='container  my-5 py-5'>
+            <div className='container my-5 py-5'>
                 <div className='row'>
                     <div className='col-12 mb-5'>
-                        <h1 className='display-6 fw-bolder text-center'>Latest Products</h1>
+                        <h1 className='display-6 fw-bolder text-center'>Produtos mais recentes</h1>
                         <hr />
                     </div>
                 </div>
